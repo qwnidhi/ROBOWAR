fbf06eebe3afd897cab2d5590ba650ff8a83ba72
aditya-1423 <av012380@gmail.com>
2024-03-04 10:48:55 +0530

    new changes

--- script.js
+++ script.js
@@ -22,19 +22,19 @@ h4all.forEach(function (elem) {
   });
 });
 
-gsap.to(".header", {
-  backgroundColor: "#000",
-  duration: 0.5,
-  height: "90px",
-  scrollTrigger: {
-    trigger: ".header",
-    scroller: "body",
-    // markers:true,
-    start: "top -10%",
-    end: "top -11%",
-    scrub: 1,
-  },
-});
+// gsap.to(".header", {
+//   backgroundColor: "#000",
+//   duration: 0.5,
+//   height: "90px",
+//   scrollTrigger: {
+//     trigger: ".header",
+//     scroller: "body",
+//     // markers:true,
+//     start: "top -10%",
+//     end: "top -11%",
+//     scrub: 1,
+//   },
+// });
 
 gsap.to("#main", {
   backgroundColor: "#000",
